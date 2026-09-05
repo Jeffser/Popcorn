838031bc25490a8d1a4bb1eefe09b19abf6e053b
Jeffser <jsamuel@tuta.io>
2026-09-05 16:24:17 -0600

    change appearance of the overview for the 1000 time today

--- src/widgets/series/overview.py
+++ src/widgets/series/overview.py
@@ -19,7 +19,7 @@ class SeriesOverview(Gtk.Overlay):
 
     @Gtk.Template.Callback()
     def format_overview(self, obj, value) -> str:
-        return '\n'.join(value.split('\n')[:2])
+        return value.split('\n')[0]
 
     @Gtk.Template.Callback()
     def format_to_bool(self, obj, value) -> bool:
